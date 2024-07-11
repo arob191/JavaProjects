4da5fd42021538a0dc85ea819ae3815d34148795
austinwrobertson98 <austinwrobertson98@gmail.com>
2024-07-10 19:47:31 -0500

    Created new Circle JavaFX project
    We will be creating a gui for a with event objects and handlers to allow
    the user to control the size of a circle object within an Hbox

---
+++ ControlCircle/src/application/ControlCircle.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.awt.Button;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class ControlCircle extends Application{
+	
+	public void start(Stage primaryStage) {
+		HBox hBox = new HBox();
+		hBox.setSpacing(10);
+		hBox.setAlignment(Pos.CENTER);
+		Button btEnlarge = new Button("Enlarge");
+		Button
+		
+	}
+
+}
---
+++ ControlCircle/src/module-info.java
@@ -0,0 +1,6 @@
+module ControlCircle {
+	requires javafx.controls;
+	requires javafx.graphics;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
