8fbe424ae8c944806b051f0dcbcd69f2ea532aec
austinwrobertson98 <austinwrobertson98@gmail.com>
2024-07-24 16:00:10 -0500

    Used file input stream and file output stream to copy images and txt
    files byte for byte

---
+++ Copy/src/copypckg/Copy.java
@@ -0,0 +1,42 @@
+package copypckg;
+
+import java.io.*;
+
+public class Copy {
+	
+	public static void main(String[] args) throws IOException {
+		if (args.length !=2) {
+			System.out.println("Usage: Java Copy sourceFile targetFile");
+			System.exit(0);
+		}
+		
+		File sourceFile = new File(args[0]);
+		if (!sourceFile.exists()) {
+			System.out.println("Source file" + args[0] + " does not exist");
+			System.exit(0);
+		}
+		
+		File targetFile = new File(args[1]);
+		if (targetFile.exists()) {
+			System.out.println("Target file" + args[1] + " already exists");
+		}
+		
+		BufferedInputStream input = new BufferedInputStream(new FileInputStream(sourceFile)); //Buffer input to make the input easier to work with
+		BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(targetFile)); //Buffer output to target file
+		
+		int r;
+		int numberOfBytesCopied = 0;
+		
+		while((r = input.read()) != -1) {
+			output.write((byte) r);
+			numberOfBytesCopied++;
+		}
+		
+		input.close();
+		output.close();
+		
+		System.out.println(numberOfBytesCopied + " bytes copied");
+		
+	}
+	
+}
