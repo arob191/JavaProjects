5ffda803060571c46ac51fb5c2dfa07968802a72
austinwrobertson98 <austinwrobertson98@gmail.com>
2024-07-24 18:04:17 -0500

    Updated formating

--- Object_IO/src/objectpckg/TestObjectInput.java
+++ Object_IO/src/objectpckg/TestObjectInput.java
@@ -1,18 +1,28 @@
+/**
+ * Objective: Use Java IO to create an objectinputstream object
+ * Algorithm: We will use the java io class create the objectinputstream object and serialize our variables our object to the "object.dat" file
+ *Input and Output: Input is our variables and our date object. Output will be the object.dat file
+ * Created by: Austin Robertson
+ * Date: 07/24/24
+ * Version: 1
+ */
+
 package objectpckg;
 
 import java.io.*;
 
 public class TestObjectInput {
 
-	public static void main(String[] args)	throws IOException, ClassNotFoundException {
+	public static void main(String[] args)	throws IOException, ClassNotFoundException { //Main argument that throws exceptions
 		
-		ObjectInputStream input = new ObjectInputStream(new FileInputStream("object.dat"));
+		ObjectInputStream input = new ObjectInputStream(new FileInputStream("object.dat")); //Create our objectinputstream object
 		
+		//Declare and serialize our variables and data object to the object.dat file
 		String name = input.readUTF();
 		double score = input.readDouble();
 		java.util.Date date = (java.util.Date) (input.readObject());
 		
-		
+		//Print out our variables and object to the user
 		System.out.println(name + " " + score + " " + date);
 		
 	}
--- Object_IO/src/objectpckg/TestObjectOutputSteam.java
+++ Object_IO/src/objectpckg/TestObjectOutputSteam.java
@@ -1,14 +1,22 @@
-
+/**
+ * Objective: Use Java IO to create an objectoutputstream object
+ * Algorithm: We will use the java io class create the objectinputstream object and serialize our variables our object to the "object.dat" file
+ *Input and Output: Input is our variables and our date object. Output will be the object.dat file
+ * Created by: Austin Robertson
+ * Date: 07/24/24
+ * Version: 1
+ */
 package objectpckg;
 
 import java.io.*;
 
 public class TestObjectOutputSteam {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException { //Main argument throws exceptions
 		
-		ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("object.dat"));
+		ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("object.dat")); //Create our objectoutputstream object
 		
+		//Output the variables and date object
 		output.writeUTF("John");
 		output.writeDouble(85.5);
 		output.writeObject(new java.util.Date());
