054ab2257eb12ccae413f3d139671b6ee8689cca
austinwrobertson98 <austinwrobertson98@gmail.com>
2024-08-19 20:42:29 -0500

    Working on loops

--- Programming Fundamentals 3/JavaTesting/src/Main.java
+++ Programming Fundamentals 3/JavaTesting/src/Main.java
@@ -2,10 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
+
+        int[] myArray = {1, 5 , 10, 7, 13, 20};
+
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] == 13) {
+                System.out.println("Found a " + myArray[i] + "!");
+            }
         }
 
 
