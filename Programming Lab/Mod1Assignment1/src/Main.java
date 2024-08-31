1a33c1e4e66a112e18b207dc524e7ff7ee6b6965
austinwrobertson98 <austinwrobertson98@gmail.com>
2024-08-30 19:16:09 -0500

    Programming Lab 1 Folder

---
+++ Programming Lab/Mod1Assignment1/src/Main.java
@@ -0,0 +1,17 @@
+//**********************************
+// COSC 1173 Programming Lab
+// Name: Austin Robertson
+// Data: 08/30/2024
+// Robertson - Assignment1: Print Information to Screen
+//**********************************
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome to my program!");
+        System.out.println("Name: Austin Robertson");
+        System.out.println("Major: Computer Science");
+        System.out.println("Hobbies: Video Games, Hiking, Spending time with my dogs");
+
+    }
+}
