414440151c05f11c4636273d3f55a7394404fc2b
austinwrobertson98 <austinwrobertson98@gmail.com>
2024-10-30 16:30:44 -0500

    MultiArrays

--- Programming Lab/MultiArray/src/Main.java
+++ Programming Lab/MultiArray/src/Main.java
@@ -1,25 +1,68 @@
+/**
+ * Class: COSC 1173 Programming Lab
+ * Name: Austin Robertson
+ * Date: 10/30/2024
+ * MultiArray: Initialize an array through user input. Calculate the sum of rows and specified column
+ */
+
 import java.util.Scanner;
 
 public class Main {
+
+    //Method accepts multidimensional arrays and returns the sum of the chosen column
+    public static double sumColumn(double[][] myArray, int column){
+        //Keep track of our total
+        double columnTotal = 0;
+
+        //Add the element of the chose column of each row to the columnTotal
+        for (int i = 0; i < 3; i++) {
+            columnTotal += myArray[i][column];
+        }
+
+        return columnTotal;
+    }
+
+    //Main code
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[][] myArray = new int[3][4];
+        //Initialize our array
+        double[][] myArray = new double[3][4];
 
+        //For loop to fill our multidimensional array
         for (int i = 0; i < myArray.length; i++){
-            System.out.println("Enter 4 integers: ");
+            //Ask user for 4 integers for each row
+            System.out.print("Enter separate 4 integers: ");
             String input = scanner.nextLine();
+            //Split the user input and insert into the string array
             String[] myStringArray = input.split(" ");
+            //For each element in the string array parse the string into a double element
             for (int j = 0; j < 4; j++){
-                myArray[i][j] = Integer.parseInt(myStringArray[j]);
+                //Parsing the string element to double and inserting into our array
+                myArray[i][j] = Double.parseDouble(myStringArray[j]);
             }
         }
 
+        //Break
+        System.out.println(" ");
+
+        //For loop to calculate the sum of each row
         for (int i = 0; i < myArray.length; i++){
+            //Keep track of our total
+            double rowTotal = 0;
+            //For each element in the first row add to the rowTotal
             for (int j = 0; j < 4; j++){
-                System.out.print(myArray[i][j] + " ");
+                rowTotal += myArray[i][j];
             }
-            System.out.println(" ");
+            System.out.println("The sum of row " + (i +1) + " is: " + rowTotal);
+        }
+
+        //Break
+        System.out.println(" ");
+
+        //Call our sumColumn method for each column
+        for (int i = 0; i < 4; i++){
+            System.out.println("The sum of column " + (i + 1) + " is: " + sumColumn(myArray, i));
         }
 
     }
