820832538fb8da47e2ad317291943359bee4b946
austinwrobertson98 <austinwrobertson98@gmail.com>
2024-10-29 20:38:14 -0500

    MultiArrays

---
+++ Programming Lab/MultiArray/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] myArray = new int[3][4];
+
+        for (int i = 0; i < 3; i++){
+            System.out.println("Enter 4 integers: ");
+            String input = scanner.nextLine();
+            String[] myStringArray = input.split(" ");
+            for (int j = 0; j < 4; j++){
+                myArray[i][j] = Integer.parseInt(myStringArray[j]);
+            }
+        }
+
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 4; j++){
+                System.out.print(myArray[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
