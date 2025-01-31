4fd9e8b77eeb8f629649282244ede8f3dfaf2fb6
austinwrobertson98 <austinwrobertson98@gmail.com>
2025-01-30 20:00:18 -0600

    Daily Java practice

---
+++ Traininng/Day1/src/Day4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Day4 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String userName = scanner.nextLine();
+
+        System.out.println("Hello " + userName);
+
+    }
+}
